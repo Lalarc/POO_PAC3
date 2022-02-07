3a090aedb9c8249e8b5c3c527bc850d03cc0a978
Lalarc <86604022+Lalarc@users.noreply.github.com>
2022-02-07 19:38:51 +0100

    Tercer ejercicio finalizado

--- PAC3Ex3/src/edu/uoc/pac3/enums/Temperature.java
+++ PAC3Ex3/src/edu/uoc/pac3/enums/Temperature.java
@@ -7,28 +7,63 @@ public enum Temperature {
 	MEDIUM(20),
 	HIGH(30);
 	
+	private int degrees;
+	
 	private Temperature(int degrees) {
-		//TODO
+		this.degrees=degrees;
+		
 	}
 	
 	public int getDegrees() {
-		//TODO
+		return degrees;
 	}
 	
 	@Override
 	public String toString() {
-		//TODO
+		return "Temperature: "+super.toString();
 	}
 	
 	public static Temperature getTemperature(int degrees) { 
-		//TODO
+		if(degrees<Temperature.LOW.getDegrees()||degrees>=Temperature.HIGH.getDegrees()+10) {
+			return Temperature.UNDEFINED;
+		} else if (degrees>=30) {
+			return Temperature.HIGH;
+		} else if (degrees>=20) {
+			return Temperature.MEDIUM;
+		} else {
+			return Temperature.LOW;
+		}
 	}
 	
 	public static Temperature[] getTemperatures(int[] degrees) {
-		//TODO
+		Temperature[] temperature = new Temperature[degrees.length];
+			for(int i=0;i<degrees.length;i++) {
+				temperature[i]=getTemperature(degrees[i]);
+			}
+			return temperature;
 	}
 	
 	public static double[] valuesInitializationIn(String scale){
-		//TODO
+		
+		double[] array = new double [Temperature.values().length];
+		int i = 0;
+		
+		if(scale.toUpperCase().equals("K")) {
+			for (Temperature t : values()) {
+				double d = t.getDegrees();
+				array[i] = d + 273.15;
+				i++;
+			}
+		}else if(scale.toUpperCase().equals("F")) {
+			for (Temperature t : values()) {
+				double d = t.getDegrees();
+				array[i] =  (d * 9 / 5) + 32;
+				i++;
+			}
+		}else {
+			array = new double[0];
+		}
+		
+		return array;
 	}
 }
